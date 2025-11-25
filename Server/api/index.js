ce1ca7f1ad2281f94cc1483f1ca1c4f71829d36e
anuragk-04 <anurag04kushwaha@gmail.com>
2025-11-26 00:49:03 +0530

    deploying

--- Server/api/index.js
+++ Server/api/index.js
@@ -5,13 +5,6 @@ import app from "../app.js";
 import connectDB from "../config/db.js";
 import serverless from "serverless-http";
 
-// Connect to DB once on cold start
 connectDB();
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
 export default serverless(app);
