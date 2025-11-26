4848156c4e42bf8700987caf20f92c9c170c19c6
anuragk-04 <anurag04kushwaha@gmail.com>
2025-11-26 18:53:17 +0530

    deploying

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -3,14 +3,14 @@ import User from "../models/User.js";
 import { generateToken } from "../utils/generateToken.js";
 
 export const register = async (req, res) => {
-  const { fullName, email, password } = req.body;
+  const { name, email, password } = req.body;
 
   const exist = await User.findOne({ email });
   if (exist) return res.status(400).json({ message: "Email already exists" });
 
   const hashed = await bcrypt.hash(password, 10);
 
-  const user = await User.create({ fullName, email, password: hashed });
+  const user = await User.create({ name, email, password: hashed });
 
   res.status(201).json({
     user,
